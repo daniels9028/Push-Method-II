2b1e3a58c1c6269b76be34182ed17ecaa784657a
daniels9028 <daniels9028@student.untan.ac.id>
2024-12-19 14:33:54 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Push-Method-II",
+  // base: "/Push-Method-II",
 });
